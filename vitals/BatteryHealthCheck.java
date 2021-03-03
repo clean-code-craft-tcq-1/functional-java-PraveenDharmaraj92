a3ad4e2308fcbc5245392c02a0d158707e537f53
PraveenDharmaraj92 <62659035+PraveenDharmaraj92@users.noreply.github.com>
2021-03-03 11:14:51 +0530

    Modified battery health check function

--- vitals/BatteryHealthCheck.java
+++ vitals/BatteryHealthCheck.java
@@ -6,8 +6,7 @@ package vitals;
 import java.util.function.Function;
 
 /**
- * @author PDH2COB
- * Function class to check battery health
+ * @author PDH2COB Function class to check battery health
  */
 public class BatteryHealthCheck implements Function<Battery, Boolean> {
 
@@ -16,41 +15,33 @@ public class BatteryHealthCheck implements Function<Battery, Boolean> {
 	 */
 	@Override
 	public Boolean apply(final Battery battery) {
-
-		boolean tempCheck;
-		boolean socCheck;
-		boolean chargeRateCheck;
-
-		tempCheck = checkTemperature(battery);
-		socCheck = checkSoc(battery);
-		chargeRateCheck = checkChargeRate(battery);
-
-		if (!tempCheck) {
-			System.out.println("Battery temperature - " + battery.getTemperature() + " not in range!");
-		}
-		if (!socCheck) {
-			System.out.println("Battery State of Charge - " + battery.getSoc() + " not in range!");
-		}
-		if (!chargeRateCheck) {
-			System.out.println("Battery Charging rate - " + battery.getChargeRate() + " not in range!");
-		}
-
-		return tempCheck && socCheck && chargeRateCheck;
+		return checkTemperature(battery) && checkSoc(battery) && checkChargeRate(battery);
 	}
 
 	private boolean checkTemperature(Battery battery) {
-		return battery.getTemperature() > BatteryHealthConstants.minTemperature
-				&& battery.getTemperature() < BatteryHealthConstants.maxTemperature;
+		if (battery.getTemperature() > BatteryHealthConstants.minTemperature
+				&& battery.getTemperature() < BatteryHealthConstants.maxTemperature) {
+			System.out.println("Battery temperature - " + battery.getTemperature() + " not in range!");
+			return false;
+		}
+		return true;
 	}
 
 	private boolean checkSoc(Battery battery) {
-		return battery.getSoc() > BatteryHealthConstants.minSoc && battery.getSoc() < BatteryHealthConstants.maxSoc;
+		if (battery.getSoc() > BatteryHealthConstants.minSoc && battery.getSoc() < BatteryHealthConstants.maxSoc) {
+			System.out.println("Battery State of Charge - " + battery.getSoc() + " not in range!");
+			return false;
+		}
+		return true;
 	}
 
 	private boolean checkChargeRate(Battery battery) {
-		return battery.getChargeRate() > BatteryHealthConstants.minChargeRate
-				&& battery.getChargeRate() < BatteryHealthConstants.maxChargeRate;
+		if (battery.getChargeRate() > BatteryHealthConstants.minChargeRate
+				&& battery.getChargeRate() < BatteryHealthConstants.maxChargeRate) {
+			System.out.println("Battery Charging rate - " + battery.getChargeRate() + " not in range!");
+			return false;
+		}
+		return true;
 	}
-	
 
 }
